ef27eebd488ddc0edbad31bd613bec53251d101f
Diman Nikolov <nikolov.m.diman@gmail.com>
2025-02-27 17:11:37 +0200

    Populated cardsData obj; cards need descriptions and base deck array needs correct quantity for each card

--- client/src/cards.js
+++ client/src/cards.js
@@ -10,27 +10,39 @@ async function createCardsAPI() {
     let cardId = 0;
     
     personCards.forEach((cardName, i) => {
-        cardsData.cards.baseGameCards.push({id: cardId, name: cardName, type: 'person', desc: '', cardUrl: `./assets/persons/${cardName}`})
+        cardsData.cards.baseGameCards.push({"id": cardId, "name": cardName, "type": 'person', "desc": '', "cardUrl": `./assets/persons/${cardName}`});
+        cardsData.baseDeckComposition.push({"cardId": cardId, "cardName": cardName, "quantity": 0});
         cardId++;
     });
 
     expeditionCards.forEach((cardName, i) => {
-        cardsData.cards.baseGameCards.push({id: cardId, name: cardName, type: 'expedition', desc: '', cardUrl: `./assets/expeditions/${cardName}`})
+        if(cardName != 'expedition-5VP-3Coin-1Anchor_1Cross_1Cabin.png') {
+            cardsData.cards.baseGameCards.push({"id": cardId, "name": cardName, "type": 'expedition', "desc": '', "cardUrl": `./assets/expeditions/${cardName}`});
+            cardsData.baseDeckComposition.push({"cardId": cardId, "cardName": cardName, "quantity": 0});
+        } else {
+            cardsData.cards.baseGameCards.push({"id": cardId, "name": cardName, "type": 'expedition', "desc": '', "cardUrl": `./assets/expeditions/${cardName}`, "minPlayers": 5});
+            cardsData.baseDeckComposition.push({"cardId": cardId, "cardName": cardName, "quantity": 1, "minPlayers": 5});
+        }
+
         cardId++;
     });
 
     shipCards.forEach((cardName, i) => {
-        cardsData.cards.baseGameCards.push({id: cardId, name: cardName, type: 'ship', desc: '', cardUrl: `./assets/ships/${cardName}`})
+        cardsData.cards.baseGameCards.push({"id": cardId, "name": cardName, "type": 'ship', "desc": '', "cardUrl": `./assets/ships/${cardName}`});
+        cardsData.baseDeckComposition.push({"cardId": cardId, "cardName": cardName, "quantity": 0});
+
         cardId++;
     });
 
     taxIncreaseCards.forEach((cardName, i) => {
-        cardsData.cards.baseGameCards.push({id: cardId, name: cardName, type: 'taxIncrease', desc: '', cardUrl: `./assets/taxIncreases/${cardName}`})
+        cardsData.cards.baseGameCards.push({"id": cardId, "name": cardName, "type": 'taxIncrease', "desc": '', "cardUrl": `./assets/taxIncreases/${cardName}`});
+        cardsData.baseDeckComposition.push({"cardId": cardId, "cardName": cardName, "quantity": 0});
+
         cardId++;
     });
     // console.log(cardsData);
-    console.log(cardsData);
-    console.log(Object.keys(cardsData).length);
+    // console.log(cardsData);
+    // console.log(Object.keys(cardsData).length);
 
 
     // await fs.writeFile('cardsData.js', JSON.stringify(cardsData), function(){});

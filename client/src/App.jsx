76aa5a3184102ef3c5b4816b0abd72ea37a7a093
Diman Nikolov <nikolov.m.diman@gmail.com>
2025-02-28 17:22:02 +0200

    finished base game deck composition and made buildDeck func in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,32 @@ import { PlayersBottom } from './PlayersBottom'
 import { cardsData } from './cardsData'
 
 
+function buildDeck(cards, deckComposition, playerCount) {
+  const deck = [];
+  
+  deckComposition.forEach(item => {
+    // Skip cards with player count requirements that aren't met
+    if (item.minPlayers && playerCount < item.minPlayers) {
+      return;
+    }
+    
+    const card = cards.find(c => c.id === item.cardId);
+    
+    for (let i = 0; i < item.quantity; i++) {
+      deck.push({...card, instanceId: `${card.id}_${i}`});
+    }
+  });
+  
+  return deck;
+}
 
+const maxPlayerBaseDeck = buildDeck(cardsData.cards.baseGameCards, cardsData.baseDeckComposition, 5); // Includes all cards
+const belowMaxPlayerBaseDeck = buildDeck(cardsData.cards.baseGameCards, cardsData.baseDeckComposition, 4);
 
 
 function App() {
   const [gamePhase, setGamePhase] = useState('Discover');
-  const [gameDeck, setGameDeck] = useState(cardsData);
+  const [gameDeck, setGameDeck] = useState(maxPlayerBaseDeck);
 
   console.log(gameDeck);
   

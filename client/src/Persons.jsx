ca8b67dcb9e53be6dcfa0fb59259acea703c398c
Diman Nikolov <nikolov.m.diman@gmail.com>
2025-03-05 17:26:12 +0200

    Unsaved file on last push for minor changes of table design

--- client/src/Persons.jsx
+++ client/src/Persons.jsx
@@ -3,7 +3,9 @@ export function Persons() {
         <>
             <div className="personsWrapper">
                 {/* <h2>Person cards segment</h2> */}
-                <div className="cardsSegment"></div>
+                <div className="cardsSegment">
+                    <div className="drawnCard"></div>
+                </div>
             </div>
         </>
     )
